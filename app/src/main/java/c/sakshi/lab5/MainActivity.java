487652211f624f803dd409c2bf7a853b88ec0deb
Jake <jparker22@wisc.edu>
2020-02-29 17:47:18 -0600

    Login activity finished

--- app/src/main/java/c/sakshi/lab5/MainActivity.java
+++ app/src/main/java/c/sakshi/lab5/MainActivity.java
@@ -1,13 +1,34 @@
 package c.sakshi.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username_et;
+    Button login_b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username_et = findViewById(R.id.username_edit_text);
+        login_b = findViewById(R.id.login_button);
+    }
+
+    public void onButtonClick(View v) {
+        String username = username_et.getText().toString();
+        if (username.length() == 0) {
+            Toast.makeText(this, "Must provide valid user name.", Toast.LENGTH_SHORT).show();
+        }
+        Intent intent = new Intent(getBaseContext(), NotesActivity.class);
+        intent.putExtra("username", username);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/c/sakshi/lab5/NotesActivity.java
@@ -0,0 +1,14 @@
+package c.sakshi.lab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+}
