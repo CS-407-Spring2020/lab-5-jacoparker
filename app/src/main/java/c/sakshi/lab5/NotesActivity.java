04f3fc39c6ea95f47185ead6d7ff1f41512144e0
Jake <jparker22@wisc.edu>
2020-02-29 18:13:46 -0600

    Complete logout button and basic menu

--- app/src/main/java/c/sakshi/lab5/NotesActivity.java
+++ app/src/main/java/c/sakshi/lab5/NotesActivity.java
@@ -1,8 +1,14 @@
 package c.sakshi.lab5;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class NotesActivity extends AppCompatActivity {
@@ -22,5 +28,25 @@ public class NotesActivity extends AppCompatActivity {
     private void showWelcomeMessage() {
         username = getIntent().getStringExtra("username");
         welcome_message_et.setText("Welcome " + username + "!");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean  onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case (R.id.new_note_option):
+                // do nothing in this milestone
+                break;
+            case (R.id.logout_option):
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+        }
+        return true;
     }
 }
