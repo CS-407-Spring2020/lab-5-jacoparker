71a152a350d9ea964ff2f0c76c35d1d715dd43b2
Jake <jparker22@wisc.edu>
2020-02-29 18:00:21 -0600

    Update activity titles and add welcome message

--- app/src/main/java/c/sakshi/lab5/NotesActivity.java
+++ app/src/main/java/c/sakshi/lab5/NotesActivity.java
@@ -3,12 +3,24 @@ package c.sakshi.lab5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NotesActivity extends AppCompatActivity {
+
+    String username;
+    TextView welcome_message_et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+
+        welcome_message_et = findViewById(R.id.welcome_text_view);
+        showWelcomeMessage();
+    }
+
+    private void showWelcomeMessage() {
+        username = getIntent().getStringExtra("username");
+        welcome_message_et.setText("Welcome " + username + "!");
     }
 }
